d9bd50db88014d0d917963fdae0a7e5ccf54808a
Nicolas Poveda <nicolaspovedas@gmail.com>
2021-11-24 18:02:36 -0500

    Reproductor funcional

--- Reproductor MP3/app.js
+++ Reproductor MP3/app.js
@@ -2,3 +2,25 @@
 //El  event listener  debe reproducir  el archivo
 // al reproducir el archivo debe  cambiar la clase del boton de play a  pause y viceversa
 
+const playButton = document.getElementById('play')
+const pauseButton = document.getElementById('pause')
+
+
+const song = new Audio('dontstopmenow.mp3')
+
+// playButton.addEventListener('click', song.play(), false )
+
+playButton.addEventListener("click", event => {
+  song.play();
+  playButton.classList.add('hide')
+  pauseButton.classList.remove('hide')
+});
+
+
+pauseButton.addEventListener("click", event => {
+    song.pause();
+    pauseButton.classList.add('hide')
+    playButton.classList.remove('hide')
+  });
+
+// song.play()
